afe9b7967a08df0394d6f7f27175784afed5d325
Xuri <Xuri@XURI-PC>
2019-04-20 19:19:27 +0200

    Add project files.

---
+++ XOR Enc/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XOR_Enc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
